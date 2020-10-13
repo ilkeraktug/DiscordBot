99685cd4f33767c79b2f740cf21c63b9c2a0ca1d
ilkeraktug <63074357+aktugilker@users.noreply.github.com>
2020-10-13 23:21:03 +0300

    Initial commit

---
+++ DC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DC
+{
+    public class Program
+    {
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		private DiscordSocketClient _client;
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+
+			_client.Log += Log;
+
+			// Remember to keep token private or to read it from an 
+			// external source! In this case, we are reading the token 
+			// from an environment variable. If you do not know how to set-up
+			// environment variables, you may find more information on the 
+			// Internet or by using other methods such as reading from 
+			// a configuration.
+			await _client.LoginAsync(TokenType.Bot,
+				"NzE2NDIzODc3ODg2MjE0MTQ1.XtLjuw.n - oxk3C9QjsOrzu1hqWB7KRZQiw");
+			await _client.StartAsync();
+
+			_client.MessageReceived += MessageReceived;
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		private async Task MessageReceived(SocketMessage message)
+		{
+			if (message.Content == "Alperen")
+			{
+				await message.Channel.SendMessageAsync("Abi beni sev!");
+				await message.Channel.SendMessageAsync(message.Timestamp.ToString());
+			}
+		}
+
+	}
+}
